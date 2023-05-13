1c85d1d0cf3edf25c4808f0521741beb4f3caec5
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-05-13 15:46:47 +0300

    tmp

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,6 @@
-export const Filter = (value, onChange) => (
+import React from 'react';
+
+export const Filter = ({ value, onChange }) => (
   <label>
     Filter by name
     <input type="text" value={value} onChange={onChange} />
